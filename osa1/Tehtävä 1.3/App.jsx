a950da2d733a08aaca010a136da1e9a1db89f0b9
eetusva <66327110+eetusva@users.noreply.github.com>
2025-04-15 02:16:24 +0300

    Add files via upload

---
+++ osa1/Tehtävä 1.3/App.jsx
@@ -0,0 +1,49 @@
+const Header = ({ course }) => <h1>{course}</h1>
+
+const Part = ({ part }) => (
+  <p>
+    {part.name} {part.exercises}
+  </p>
+)
+
+const Content = ({ parts }) => (
+  <div>
+    <Part part={parts[0]} />
+    <Part part={parts[1]} />
+    <Part part={parts[2]} />
+  </div>
+)
+
+const Total = ({ parts }) => (
+  <p>
+    Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}
+  </p>
+)
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+
+  const parts = [part1, part2, part3]
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total parts={parts} />
+    </div>
+  )
+}
+
+export default App
