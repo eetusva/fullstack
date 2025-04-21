cb7ce37705987db056179684513592a508921722
eetusva <66327110+eetusva@users.noreply.github.com>
2025-04-22 00:59:27 +0300

    Add files via upload

---
+++ osa1/Tehtävä 1.10/Statistics.jsx
@@ -0,0 +1,20 @@
+import StatisticLine from './StatisticLine'
+
+const Statistics = ({ good, neutral, bad }) => {
+  const total = good + neutral + bad
+  const average = total === 0 ? 0 : (good - bad) / total
+  const positive = total === 0 ? 0 : (good / total) * 100
+
+  return (
+    <div>
+      <StatisticLine text="good" value={good} />
+      <StatisticLine text="neutral" value={neutral} />
+      <StatisticLine text="bad" value={bad} />
+      <StatisticLine text="total" value={total} />
+      <StatisticLine text="average" value={average} />
+      <StatisticLine text="positive" value={positive + ' %'} />
+    </div>
+  )
+}
+
+export default Statistics
---
+++ osa1/Tehtävä 1.14/Statistics.jsx
@@ -0,0 +1,22 @@
+import StatisticLine from './StatisticLine'
+
+const Statistics = ({ good, neutral, bad }) => {
+  const total = good + neutral + bad
+  const average = total === 0 ? 0 : (good - bad) / total
+  const positive = total === 0 ? 0 : (good / total) * 100
+
+  return (
+    <table>
+      <tbody>
+        <StatisticLine text="good" value={good} />
+        <StatisticLine text="neutral" value={neutral} />
+        <StatisticLine text="bad" value={bad} />
+        <StatisticLine text="all" value={total} />
+        <StatisticLine text="average" value={average} />
+        <StatisticLine text="positive" value={positive + ' %'} />
+      </tbody>
+    </table>
+  )
+}
+
+export default Statistics
